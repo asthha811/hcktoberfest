a34d56d214d5e7a8d23c58fcafa5aeb84fbaacc2
Asthha Wahal <64919910+asthha811@users.noreply.github.com>
2020-10-15 11:04:37 +0530

    Update base64_cipher.java
    added comments

--- base64_cipher.java
+++ base64_cipher.java
@@ -4,10 +4,13 @@
 * so that encoded textual data can be easily transported over network un-corrupted and without any data loss.(
 * @Code by Rishabhpatel803
 */
+//package declaration
 import java.util.*;
+//class decalration
 public class base64_cipher {
 
     //encrypt function to encrypt the original message
+    //method defination
     public static String encrypt(String message)
     {
         //Base64 class function used to encrypt the string
@@ -15,6 +18,8 @@ public class base64_cipher {
         //and encodeToString() is encoding the received message to string
         //getBytes() is used to convert original message to byte
         String cipher=Base64.getEncoder().encodeToString(message.getBytes());
+        
+        // return
         return cipher;
     }
 
@@ -26,19 +31,22 @@ public class base64_cipher {
         byte[] actualmsg=Base64.getDecoder().decode(message);
         //converting byte array to string
         String plain=new String(actualmsg);
+        //return
         return plain;
     }
 
+    //main method
     public static void main(String[] args) {
+        //object declaration
         Scanner sc=new Scanner(System.in);
-        
+        //print statement
         System.out.print("Enter text to be encrypted : ");
         String plainMSG=sc.nextLine();
         String cipher=encrypt(plainMSG);
-        
+        //print statement
         System.out.println("Encrypted Message : "+cipher);
         System.out.println("Decrypted Message : "+decrypt(cipher));
-        
+        //scanner closes
         sc.close();
     }
 }
